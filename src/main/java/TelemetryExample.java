dd80a1e4411cab21aadfbba4c1e3dd5657015ad5
Rohit Kumar Singh <49697906+rohity294@users.noreply.github.com>
2025-09-09 18:52:02 +0000

    c6

--- src/main/java/TelemetryExample.java
+++ src/main/java/TelemetryExample.java
@@ -1,23 +1,45 @@
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 import java.util.Random;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class TelemetryExample {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
         PrometheusMeterRegistry registry = new PrometheusMeterRegistry(io.micrometer.prometheus.PrometheusConfig.DEFAULT);
         Random random = new Random();
-
-        Random rand = new Random();
-        int min = 1;
-        int max = 10;
-        int randomNum = rand.nextInt((max - min) + 1) + min;
 
         Gauge.builder("temperature_celsius", () -> random.nextInt(30 - 20 + 1) + 20)
              .description("Temperature in Celsius")
              .register(registry);
 
+        // Start HTTP server on port 8080
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/metrics", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) {
+                try {
+                    String response = registry.scrape();
+                    exchange.getResponseHeaders().set("Content-Type", "text/plain; version=0.0.4");
+                    exchange.sendResponseHeaders(200, response.length());
+                    OutputStream os = exchange.getResponseBody();
+                    os.write(response.getBytes());
+                    os.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        server.setExecutor(null);
+        server.start();
+
+        System.out.println("Metrics server running at http://localhost:8080/metrics");
+
+        // Keep the app running
         while (true) {
-            System.out.println(registry.scrape());
             Thread.sleep(5000);
         }
     }
