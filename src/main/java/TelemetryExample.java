dd7affdb8fb3674199271df6eadc53abad74e583
Rohit Kumar Singh <49697906+rohity294@users.noreply.github.com>
2025-09-09 18:26:07 +0000

    c4

--- src/main/java/TelemetryExample.java
+++ src/main/java/TelemetryExample.java
@@ -7,6 +7,11 @@ public class TelemetryExample {
         PrometheusMeterRegistry registry = new PrometheusMeterRegistry(io.micrometer.prometheus.PrometheusConfig.DEFAULT);
         Random random = new Random();
 
+        Random rand = new Random();
+        int min = 1;
+        int max = 10;
+        int randomNum = rand.nextInt((max - min) + 1) + min;
+
         Gauge.builder("temperature_celsius", () -> random.nextInt(20, 30))
              .description("Temperature in Celsius")
              .register(registry);
